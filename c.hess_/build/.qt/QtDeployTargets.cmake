cd80dc65ec42d09bcdee2f8b81fa9c00200a429b
Tasitakara <tasit_akara@cmu.ac.th>
2026-03-05 14:04:17 +0700

    FINALLLLLYYYY config DONE

---
+++ c.hess_/build/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_chess_FILE D:/WTF/Project/c.hess_/build/chess.exe)
+set(__QT_DEPLOY_TARGET_chess_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_chess_RUNTIME_DLLS C:/Qt/6.10.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.10.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.10.2/mingw_64/bin/Qt6Core.dll)
--- c.hess_/chess.cpp
+++ c.hess_/chess.cpp
@@ -1,16 +1,37 @@
 #include <QApplication>
 #include <QPushButton>
+#include <QLabel>
+#include <QWidget>
+#include <QLayout>
+#include <QLayoutItem>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QObject>
+#include <QDebug>
+
+
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+    
+    QVBoxLayout start_layout;
+    QWidget window;
 
-    QPushButton button("Hello Qt!");
-    button.resize(300, 100);
-    button.show();
+// Create a label and two buttons for the chess game
+    QLabel label("Chess Game");
+    label.resize(1000, 1600);
+    label.show();
+
+    QPushButton startButton("Start");
+    QPushButton exitButton("Exit");
+
+    start_layout.addWidget(&label);
+    start_layout.addWidget(&startButton);
+    start_layout.addWidget(&exitButton);
+
+    window.setLayout(&start_layout);
+    window.show();
 
     return app.exec();
-
-
-    
 }
